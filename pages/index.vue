cf0e9cf1c65219e71dd6173eaa82cb5781e58d15
tri le <letrin93@gmail.com>
2023-11-25 14:48:10 -0700

    setup

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="component p-10 bg-red-500 w-fit">
+    <BlogCard/>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+
+</script>
+
+
+<style lang="css" scoped>
+
+</style>
